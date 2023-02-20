236ca62902dc12687ab9c535431269045bd3638f
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2023-02-20 16:28:19 +0200

    additional config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 255,
+  blue: 255,
+  green: 255,
+};
+
+console.log(color);
+
+export default color;
