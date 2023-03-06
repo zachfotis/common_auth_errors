fc6944264ab8260a7a31c554c051f6c07c5e1bd0
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2023-03-06 22:20:40 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Re-export all the files in the common folder
 export * from './errors/bad-request-error';
 export * from './errors/custom-error';
 export * from './errors/database-connection-error';
