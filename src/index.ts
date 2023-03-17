4442e45fc7f0a1cde3e1a9d848310c21a7799a47
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2023-03-17 23:16:01 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,5 @@ export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
+export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
